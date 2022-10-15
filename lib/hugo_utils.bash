8fc251dae67c5f8e74da97e1a77cbf1739fc563d
Edditoria <edditoria@pm.me>
2022-10-15 15:12:57 +0800

    Add CSV of release data; Add support of download <= v0.102

--- lib/hugo_utils.bash
+++ lib/hugo_utils.bash
@@ -36,31 +36,54 @@ get_arch() {
   echo "$arch"
 }
 
-# TODO: Temp only work with Hugo `0.104.*`.
 # Returns {string} Example: "https://github.com/gohugoio/hugo/releases/download/v0.104.3/hugo_extended_0.104.3_darwin-universal.tar.gz".
 create_release_url() {
   local gh_repo="$1" # $GH_REPO in utils.bash file.
   local version="$2" # Requested $version in utils.bash file. Example: "extended-0.104.3".
 
-  local release_version="${version#extended-}"
-  local file_version="${version/#extended-/extended_}"
-
+  local current_script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
+  local release_version="${version#extended-}" # Example: 0.104.3
   local kernel="$(get_kernel)" # aka the GOOS.
   local arch="$(get_arch)"     # aka the GOARCH.
+
   if [[ "$kernel" == unsupported* ]]; then
     echo "Detected kernel \"$(uname -s)\" but not supported."
-    return 1
+    return 1 # early.
   fi
   if [[ "$arch" == unsupported* ]]; then
     echo "Detected arch \"$(uname -m)\" but not supported."
-    return 1
+    return 1 # early.
   fi
+
+  # If version <= 0.102:
+
+  local major_version=$(echo $release_version | cut -d. -f1)
+  local minor_version=$(echo $release_version | cut -d. -f2)
+  local csv_file_path="$current_script_dir/hugo_releases.csv"
+  if [[ "$major_version" -eq 0 && "$minor_version" -lt 103 ]]; then
+    local ex
+    [[ "$version" == extended* ]] && ex='hasEx' || ex='noEx'
+    local query_keyword="${kernel},${arch},${ex},${release_version}"
+    # echo "Query keyword: $query_keyword"
+    local query_result="$(cat "$csv_file_path" | grep "$query_keyword" | head -1)"
+    if [[ -z $query_result ]]; then
+      echo 'Version not exist in Hugo release'
+      return 1 # early.
+    fi
+    # echo "Query result: $query_result"
+    local filename="${query_result#$query_keyword\,}"
+    echo "${gh_repo}/releases/download/v${release_version}/${filename}"
+    return # early.
+  fi
+
+  # Else case:
 
   # Fix for macOS:
   if [[ "$kernel-$arch" == 'darwin-amd64' || "$kernel-$arch" == 'darwin-arm64' ]]; then
     arch="universal"
   fi
 
+  local file_version="${version/#extended-/extended_}"
   # Example: "hugo_extended_0.104.3_darwin-universal.tar.gz"
   local filename="hugo_${file_version}_${kernel}-${arch}.tar.gz"
 
